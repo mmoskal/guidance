406f2d537d46c2929e90630aa099e4db1dea256c
Michal Moskal <michal@moskal.me>
2024-04-06 01:07:04 +0000

    wrap results

--- guidance/_rust/src/lib.rs
+++ guidance/_rust/src/lib.rs
@@ -1,6 +1,9 @@
-use std::{fmt::Display, sync::Mutex};
+use std::{
+    fmt::Display,
+    sync::{Arc, Mutex},
+};
 
-use aici_abi::{self_seq_id, MidProcessArg};
+use aici_abi::{svob::SimpleVob, toktree::TokTrie, MidProcessArg, ProcessResultOffset};
 use aici_guidance_ctrl::TokenParser;
 use aici_native::bintokens::ByteTokenizerEnv;
 use pyo3::{exceptions::PyValueError, prelude::*};
@@ -8,6 +11,13 @@ use pyo3::{exceptions::PyValueError, prelude::*};
 #[pyclass]
 struct Engine {
     parser: Mutex<TokenParser>,
+    tok_trie: Arc<TokTrie>,
+}
+
+#[pyclass]
+struct TokenMask {
+    inner: Mutex<SimpleVob>,
+    tok_trie: Arc<TokTrie>,
 }
 
 fn val_error(e: impl Display) -> PyErr {
@@ -19,22 +29,50 @@ impl Engine {
     #[new]
     fn py_new(tokenizer_name: &str, protobuf: &[u8]) -> PyResult<Self> {
         let env = ByteTokenizerEnv::load(tokenizer_name).map_err(val_error)?;
+        let tok_trie = env.tok_trie.clone();
         let parser =
             TokenParser::from_guidance_protobuf(Box::new(env), protobuf).map_err(val_error)?;
         Ok(Engine {
             parser: Mutex::new(parser),
+            tok_trie: Arc::new(tok_trie),
         })
     }
 
-    fn mid_process(self_: PyRef<'_, Self>) -> PyResult<String> {
+    fn mid_process(&self, backtrack: u32, tokens: Vec<u32>) -> PyResult<(Vec<TokenMask>, String)> {
         let arg = MidProcessArg {
-            backtrack: 0,
-            tokens: vec![],
-            fork_group: vec![self_seq_id()],
+            backtrack,
+            tokens,
+            fork_group: vec![],
         };
-        let mut p = self_.parser.lock().unwrap();
-        let _r = p.mid_process(arg);
-        Ok("mid_process".to_string())
+        let mut p = self.parser.lock().unwrap();
+        let r = p.mid_process(arg);
+        let mut token_sets = Vec::new();
+        let r2 = ProcessResultOffset {
+            branches: r
+                .branches
+                .iter()
+                .map(|b| {
+                    b.map_mask(|x| {
+                        let idx = token_sets.len();
+                        token_sets.push(TokenMask {
+                            inner: Mutex::new(x.clone()),
+                            tok_trie: self.tok_trie.clone(),
+                        });
+                        idx
+                    })
+                })
+                .collect(),
+        };
+        let s = serde_json::to_string(&r2).map_err(val_error)?;
+        Ok((token_sets, s))
+    }
+}
+
+#[pymethods]
+impl TokenMask {
+    fn __repr__(&self) -> PyResult<String> {
+        let ts = self.inner.lock().unwrap();
+        Ok(self.tok_trie.token_set_dbg(&ts))
     }
 }
 
@@ -52,5 +90,6 @@ fn guidancerust(m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     m.add_function(wrap_pyfunction!(engine_start, m)?)?;
     m.add_class::<Engine>()?;
+    m.add_class::<TokenMask>()?;
     Ok(())
 }
--- mtest.py
+++ mtest.py
@@ -1,20 +1,70 @@
-from guidance._rust.guidancerust import engine_start, Engine
+from guidance._rust.guidancerust import engine_start, Engine, TokenMask
 from guidance import select, capture, gen
+from dataclasses import dataclass, field
+import json
+
+
+@dataclass
+class Splice:
+    # If one of the tokens in when_sampled is sampled, this sequence is appended.
+    # When empty, this sequence is appended unconditionally, regardless of sampling.
+    ff_tokens: list[int]
+    # Backtrack this much before appending this sequence (this includes sampled token if any).
+    when_sampled: list[int]
+    # Append these tokens after backtracking.
+    backtrack: int
+
+
+@dataclass
+class Branch:
+    mask: TokenMask | None = None
+    splices: list[Splice] = field(default_factory=list)
+
+
+class Engine2:
+    def __init__(self, tokenizer_name, grammar):
+        self.engine = Engine(tokenizer_name, grammar.serialize())
+
+    def process(self, backtrack: int, tokens: list[int]):
+        token_sets, res_str = self.engine.mid_process(backtrack, tokens)
+        r = json.loads(res_str)
+        if len(r["branches"]) == 0:
+            return None  # stop
+        assert len(r["branches"]) == 1, "forking not expected"
+        b: dict = r["branches"][0]
+        rb = Branch()
+        mask_idx = b.get("sample_mask", None)
+        if mask_idx is not None:
+            rb.mask = token_sets[mask_idx]
+        for sp in b["splices"]:
+            rb.splices.append(
+                Splice(
+                    ff_tokens=sp["ff_tokens"],
+                    when_sampled=sp["when_sampled"],
+                    backtrack=sp["backtrack"],
+                )
+            )
+        return rb
+
 
 def main():
     grm = (
         "<joke>Parallel lines have so much in common. It’s a shame they’ll never meet.</joke>\n"
         + "<joke>"
-        + capture(gen(regex=r'[A-Z\(].*', stop="</joke>"), "joke")
+        + capture(gen(regex=r"[A-Z\(].*", stop="</joke>"), "joke")
         + "</joke>\nScore (of 10): "
         + capture(gen(regex=r"\d{1,3}"), "score")
         + "\n"
     )
-    b = grm.serialize()
-    e = Engine("llama", b)
-    print(e)
-    s = engine_start("fo", "bar", False)
-    print(s)
+    e = Engine2("llama", grm)
+    r0 = e.process(0, [])
+    print(r0)
+    tokens = r0.splices[0].ff_tokens
+    r1 = e.process(0, tokens)
+    print(r1)
+
+    # s = engine_start("fo", "bar", False)
+    # print(s)
+
 
 main()
-
